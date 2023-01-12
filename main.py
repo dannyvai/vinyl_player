569dc4534df0c92fe8b45c0a5c0ee181cc0198eb
dannyvai <danny.wainshtein@gmail.com>
2023-01-13 01:53:34 +0200

    move recorder code to recorder.py

--- main.py
+++ main.py
@@ -6,15 +6,6 @@ import serial
 from pathlib import Path
 from collections import defaultdict
 
-use_camera = False
-if use_camera:
-    cam = cv2.VideoCapture(0)
-    videogram_serial = serial.Serial('COM6', baudrate=9600)
-
-else:
-    img = cv2.imread("audio_images/img.png")
-
-
 record_circumference_mm = 957.557
 RPM = 100.0/3.0
 RPS = RPM/60.0
@@ -27,58 +18,48 @@ kernel = np.ones((5, 5), np.uint8)
 signals = defaultdict(list)
 
 for i in range(2038//2):
-    if use_camera:
-        ret,img = cam.read()
-        if not ret:
-            continue
-        # cv2.imwrite("img.png",img)
-        cv2.imshow("img",img)
-        cv2.waitKey(300)
-        cv2.imwrite('img_{:04d}.png'.format(i),img)
-        videogram_serial.write(b'A')
-    else:
-        if i > 100:
-            break
-        image_path = Path("audio_images")/"img_{:04d}.png".format(i)
-        print(image_path)
-        img = cv2.imread(str(image_path))
-        if img is None:
-            print(f"Can't load {image_path}")
-            continue
-        gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        ret,bin_img = cv2.threshold(gray_img,25,255,cv2.THRESH_BINARY_INV)
-        # bin_img = cv2.erode(bin_img, kernel)
+    if i > 100:
+        break
+    image_path = Path("audio_images")/"img_{:04d}.png".format(i)
+    print(image_path)
+    img = cv2.imread(str(image_path))
+    if img is None:
+        print(f"Can't load {image_path}")
+        continue
+    gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret,bin_img = cv2.threshold(gray_img,25,255,cv2.THRESH_BINARY_INV)
+    # bin_img = cv2.erode(bin_img, kernel)
 
-        # Apply the Component analysis function
-        analysis = cv2.connectedComponentsWithStats(bin_img,
-                                                    4,
-                                                    cv2.CV_32S)
-        (totalLabels, label_ids, values, centroid) = analysis
+    # Apply the Component analysis function
+    analysis = cv2.connectedComponentsWithStats(bin_img,
+                                                4,
+                                                cv2.CV_32S)
+    (totalLabels, label_ids, values, centroid) = analysis
 
-        # Initialize a new image to
-        # store all the output components
-        output = np.zeros(gray_img.shape, dtype="uint8")
-        lines_count = 0
-        for i in range(1, totalLabels):
-            area = values[i, cv2.CC_STAT_AREA]
+    # Initialize a new image to
+    # store all the output components
+    output = np.zeros(gray_img.shape, dtype="uint8")
+    lines_count = 0
+    for i in range(1, totalLabels):
+        area = values[i, cv2.CC_STAT_AREA]
 
-            if (area > 5000) :
+        if (area > 5000) :
 
-                # Labels stores all the IDs of the components on the each pixel
-                # It has the same dimension as the threshold
-                # So we'll check the component
-                # then convert it to 255 value to mark it white
-                componentMask = (label_ids == i).astype("uint8") * 255
-                data = (np.argmax(componentMask,axis=0)+480-np.argmax(componentMask[::-1],axis=0))/2
-                # Creating the Final output mask
-                output = cv2.bitwise_or(output, componentMask)
+            # Labels stores all the IDs of the components on the each pixel
+            # It has the same dimension as the threshold
+            # So we'll check the component
+            # then convert it to 255 value to mark it white
+            componentMask = (label_ids == i).astype("uint8") * 255
+            data = (np.argmax(componentMask,axis=0)+480-np.argmax(componentMask[::-1],axis=0))/2
+            # Creating the Final output mask
+            output = cv2.bitwise_or(output, componentMask)
 
-                # plt.plot(data)
-                signals[lines_count].append(data)
-                lines_count += 1
+            # plt.plot(data)
+            signals[lines_count].append(data)
+            lines_count += 1
 
-        # plt.imshow(label_ids, cmap='gray')
-        # plt.show()
+    # plt.imshow(label_ids, cmap='gray')
+    # plt.show()
 
 window = 180
 for i in signals.keys():
---
+++ recorder.py
@@ -0,0 +1,20 @@
+import cv2
+import serial
+import os
+
+cam = cv2.VideoCapture(0)
+videogram_serial = serial.Serial('COM6', baudrate=9600)
+
+arduino_step_size = 2
+vinyl_images_basedir = "vinyl_images"
+os.makedirs(vinyl_images_basedir)
+
+for i in range(2038//arduino_step_size):
+    ret, img = cam.read()
+    if not ret:
+        continue
+    cv2.imshow("img", img)
+    cv2.waitKey(300)
+    cv2.imwrite(os.path.join(vinyl_images_basedir,'img_{:04d}.png'.format(i)),img)
+    videogram_serial.write(b'A')
+
